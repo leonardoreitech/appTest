56d820558edccd7e3c084a3d165239d13cc70c30
leonardoreitech <leonardo@reitech.com.br>
2024-01-29 07:50:15 -0300

    first commit

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace jenkinsAndroid;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
