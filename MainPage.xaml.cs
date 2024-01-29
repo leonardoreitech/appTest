f3b52ee95a7c4044d9dc3d00b83b148d6c7bbda7
leonardoreitech <leonardo@reitech.com.br>
2024-01-29 08:03:48 -0300

    criando ref

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,41 @@
-﻿namespace jenkinsAndroid;
+﻿using Library;
+
+namespace jenkinsAndroid;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
 
+	private Class1 Class;
+	public Class1 classs
+	{
+		get => Class;
+		set
+		{
+			Class = value;
+			OnPropertyChanged();
+		}
+	}
+	private string _testBinding;
+	public string C_testBinding
+	{
+		get => _testBinding;
+		set
+		{
+			_testBinding = value;
+			OnPropertyChanged();
+		}
+	}
+
 	public MainPage()
 	{
 		InitializeComponent();
+		BindingContext = this;
+
+		classs = new();
+		classs.name = "Bem vindo";
+
+		C_testBinding = classs.name;
 	}
 
 	private void OnCounterClicked(object sender, EventArgs e)
